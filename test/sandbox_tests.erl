e7ea5bee04d098c550144fc8f32e356003ab5bdb
Roberto Aloi <prof3ta@gmail.com>
2012-04-11 11:47:31 +0200

    Fix tests after recent changes to the restrictions.

--- test/sandbox_tests.erl
+++ test/sandbox_tests.erl
@@ -9,7 +9,7 @@ fail_test() ->
     ?assertException(error, {restricted, _}, eval_os()).
 
 zipwith_and_exec_test() ->
-    ?assertException(error, {restricted, _}, eval_zipwith_and_exec()).
+    ?assertException(error, {restricted}, eval_zipwith_and_exec()).
 
 apply_test() ->
     ?assertException(error, {restricted, _}, eval_apply()).
@@ -23,6 +23,12 @@ local_test() ->
 bindings_test() ->
     ?assertException(error, {restricted, _}, eval_bindings()).
 
+seq_test() ->
+    ?assertMatch({[1,2,3,4,5], _}, eval_good_seq_2()),
+    ?assertMatch({[5,4,3,2,1], _}, eval_good_seq_3()),
+    ?assertException(error, {restricted, _}, eval_bad_seq_2()),
+    ?assertException(error, {restricted, _}, eval_bad_seq_3()).
+
 %% Internal
 eval_sort() ->
     sandbox:eval("lists:sort([3,2,1]).").
@@ -32,9 +38,7 @@ eval_os() ->
 
 eval_zipwith_and_exec() ->
     E1 = "F = fun (M,F,A) -> lists:zipwith3({erlang, apply}, [M], [F], [ A ]) end.",
-    {_, Bs} = sandbox:eval(E1, []),
-    E2 = "F(init, stop, []).",
-    sandbox:eval(E2, Bs).
+    sandbox:eval(E1, []).
 
 eval_apply() ->
     sandbox:eval("erlang:apply(os, cmd, [foo]).").
@@ -46,7 +50,23 @@ eval_apply_in_fun_and_exec() ->
     sandbox:eval(E2, Bs).
 
 eval_local() ->
-    sandbox:eval("now().").
+    sandbox:eval("term_to_binary(pigeon).").
 
 eval_bindings() ->
     sandbox:eval("b().").
+
+eval_good_seq_2() ->
+    E1 = "lists:seq(1, 5).",
+    sandbox:eval(E1).
+
+eval_good_seq_3() ->
+    E1 = "lists:seq(5, 1, -1).",
+    sandbox:eval(E1).
+
+eval_bad_seq_2() ->
+    E1 = "lists:seq(1, 250).",
+    sandbox:eval(E1).
+
+eval_bad_seq_3() ->
+    E1 = "lists:seq(250, 1, -1).",
+    sandbox:eval(E1).
